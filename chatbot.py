32f978a2e68b24ee0c32a7e5cb886ba7de90d5c0
BrandonEmbleton <brandonembleton04@gmail.com>
2025-12-10 17:34:46 -0500

    Add bot instrucitons

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -15,6 +16,12 @@ while(True):
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
+        config=types.GenerateContentConfig(
+            system_instruction='''You are a chinese practice partner, respond 
+            using HSK1 level chinese along with a translation. Respond in english
+            when the user makes spelling or grammar mistakes explaining how they are
+            incorrect. '''
+        ),
         contents=userResponse,
     )
 
